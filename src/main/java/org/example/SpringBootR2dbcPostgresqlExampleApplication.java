1ab9d45ba699622616d9420cb8a741fcb425b970
md.shahbazalam <md.shahbazalam@phonepe.com>
2025-01-10 21:19:51 +0530

    initial commit

---
+++ src/main/java/org/example/SpringBootR2dbcPostgresqlExampleApplication.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableR2dbcRepositories
+@SpringBootApplication
+@EnableWebFlux
+public class SpringBootR2dbcPostgresqlExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootR2dbcPostgresqlExampleApplication.class, args);
+    }
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+        return initializer;
+    }
+}
---
+++ src/main/java/org/example/model/Tutorial.java
@@ -0,0 +1,62 @@
+package org.example.model;
+import org.springframework.data.annotation.Id;
+
+
+public class Tutorial {
+
+    @Id
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private boolean published;
+
+    public Tutorial() {
+
+    }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public void setPublished(final boolean published) {
+        this.published = published;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
+}
